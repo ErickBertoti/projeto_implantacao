7197c9dea98c92e2c4eb6050a1b4f956c141c7a1
ErickBertoti <erickhoffmann2301@gmail.com>
2025-04-01 19:59:41 -0300

    Initial Commit

---
+++ apiuser_back/src/main/java/com/senai/user/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.senai.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${apiuser.urlcors.allow}")
+    private String servFront_Porta;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("http://189.8.205.53/sauber")); // Ajustado para o Virtual Site
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
